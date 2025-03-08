5ad7127bbabd445f99a5a61c4ecb31c78b86777f
jannatulFerdeous <angeljannat673@gmail.com>
2025-03-08 22:54:27 +0600

    Refactor App component for improved readability and consistency

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { NavBar } from './components/Navbar';
-import { Banner } from './components/Banner';
-import { About } from './components/About';
-import { Skills } from './components/Skills';
-import { Project } from './components/Project';
-import { Contact } from './components/Contact';
-import { Footer } from './components/Footer';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavBar } from "./components/Navbar";
+import { Banner } from "./components/Banner";
+import { About } from "./components/About";
+import { Skills } from "./components/Skills";
+import { Project } from "./components/Project";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 
-
-function App(){
-    return (
-        <div>
-            <NavBar/>
-            <Banner/>
-            <About/>
-            <Skills/>
-            <Project/>
-            <Contact/>
-            <Footer/>
-        </div>
-    ); 
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Banner />
+      <About />
+      <Skills />
+      <Project />
+      <Contact />
+      <Footer />
+      hh
+    </div>
+  );
 }
 
-export default App; 
+export default App;
